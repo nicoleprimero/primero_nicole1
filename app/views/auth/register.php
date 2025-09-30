00015f4fa0bb37d194808bf57c0ac4c1fa2bd0f8
Nicole Primero <nicoleprimero20@gmail.com>
2025-09-30 12:40:32 +0800

    First Commit

--- app/views/auth/register.php
+++ app/views/auth/register.php
@@ -29,6 +29,9 @@
                 <div class="form-group">
                     <label for="password">Password</label>
                     <input id="password" type="password" name="password" placeholder="Enter password" required>
+
+                    
+                    <input type="password" name="password_confirmation" placeholder="Confirm Password" required>
                 </div>
 
                 <!-- Role -->
